e10591f75ccdd2395f599c0ab74083f984289427
Sarah Asare-Darko <saradarko22@gmail.com>
2024-06-12 10:04:12 +0000

    changes from MAC

--- js/index.js
+++ js/index.js
@@ -4,4 +4,42 @@ btn.onclick = function () {
     alert('Donate money');
 }
 
+const buttonLeft = document.getElementById('left');
+buttonLeft.onclick = function () {
+    document.getElementById('parent').scrollLeft -= 300;
+    
+};
+
+
+const buttonRight = document.getElementById('right');
+buttonRight.onclick = function () {
+    document.getElementById('parent').scrollLeft += 300;
+};
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     const parent = document.getElementById('parent');
+//     const leftButton = document.getElementById('left');
+//     const rightButton = document.getElementById('right');
+
+//     const scrollAmount = 300; // Adjust this value as needed
+
+//     leftButton.addEventListener('click', () => {
+//         parent.scrollBy({
+//             left: -scrollAmount,
+//             behavior: 'smooth'
+//         });
+//     });
+
+//     rightButton.addEventListener('click', () => {
+//         parent.scrollBy({
+//             left: scrollAmount,
+//             behavior: 'smooth'
+//         });
+//     });
+// });
 
